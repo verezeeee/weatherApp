5b443652e5ece9fb556c965acb98cd80cacf2086
verezeeee <theusma58@gmiaal.com>
2024-05-12 15:19:47 -0300

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Matheus on 12/05/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
